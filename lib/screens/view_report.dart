ec80c996e80dffd37a51092127801c36edb46bb0
Syahrin Seth <syahrinsethservices@gmail.com>
2021-08-05 21:15:00 +0800

    Update: dhikr history title

--- lib/screens/view_report.dart
+++ lib/screens/view_report.dart
@@ -63,7 +63,44 @@ class _ViewReport extends State<ViewReport>
               width: width,
               child: Column(
                 children: <Widget>[
-                  MyBackButton(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Hero(
+                        tag: 'backButton',
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              size: 25,
+                              color: Color(0xff3d7068),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text('Dhikr History'),
+                      Hero(
+                        tag: 'More',
+                        child: GestureDetector(
+                          onTap: () {
+                            // Navigator.pop(context);
+                          },
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Icon(
+                              Icons.info,
+                              size: 25,
+                              color: Color(0xff43c59e),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                   // SizedBox(
                   //   height: 10,
                   // ),
