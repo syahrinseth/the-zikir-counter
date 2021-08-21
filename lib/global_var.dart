00ebbff0560199718f74c1bc3581a354e0b0cc73
Syahrin Seth <syahrinsethservices@gmail.com>
2021-08-22 00:00:14 +0800

    Prod Ads Banner Id

--- lib/global_var.dart
+++ lib/global_var.dart
@@ -3,5 +3,6 @@ class GlobalVar {
       '<div>Icons made by <a href="https://www.flaticon.com/authors/ddara" title="dDara">dDara</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>';
   String avatarMaleAttr =
       '<div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>';
-  static String bannerAdId = "ca-app-pub-3940256099942544/6300978111";
+  // static String bannerAdId = "ca-app-pub-3940256099942544/6300978111"; // test
+  static String bannerAdId = "ca-app-pub-7782324487799920/5930999559"; // prod
 }
